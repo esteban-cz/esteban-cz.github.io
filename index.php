d592397c9d0d4564ec619ca9c3281ed3c1312c95
tomeckas <tomeckas@ssemi.cz>
2022-10-04 13:56:47 +0200

    new

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <title>Stránka pana Tomečky</title>
+</head>
+<body>
+
+<form action="index.html" method="post">
+    <dl>
+        <dt>Chete se vratit zpet?</dt>
+        <input type="radio" name="ano/ne" value="1" >Ano
+        <input type="radio" name="ano/ne" value="2" >Ne
+        <input type="submit" value="Submit">
+    </dl>
+</form>
+</body>
+</html>
+
