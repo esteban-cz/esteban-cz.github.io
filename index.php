3f624d5f76d7a4d09feedad05d32792f8c9e9da3
tomeckas <tomeckas@ssemi.cz>
2022-10-11 13:19:20 +0200

    new

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Stránka pana Tomečky</title>
+    <link rel="website icon" type="png" href="it2/images/web-icon.png">
 </head>
-<body>
+<body style="background-color:black; color: magenta;">
 
 <form action="index.html" method="post">
-    <input type="submit" value="Zpět">
+    <input type="image" src="it2/images/back.png" width="5%" height="5%">
 
 </form>
 </body>
