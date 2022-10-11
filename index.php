5e4a30806cc11e7d3abbd6ec9b8ac3fc12967554
tomeckas <tomeckas@ssemi.cz>
2022-10-11 10:10:40 +0200

    new

--- index.php
+++ index.php
@@ -7,12 +7,8 @@
 <body>
 
 <form action="index.html" method="post">
-    <dl>
-        <dt>Chete se vratit zpet?</dt>
-        <input type="radio" name="ano/ne" value="1" >Ano
-        <input type="radio" name="ano/ne" value="2" >Ne
-        <input type="submit" value="Submit">
-    </dl>
+    <input type="submit" value="Zpět">
+
 </form>
 </body>
 </html>
