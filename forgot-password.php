6e54c39f0b251d284a45db1ba00d163d5c867ded
tomeckas <tomeckas@ssemi.cz>
2022-10-15 11:07:55 +0200

    new

---
+++ forgot-password.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+    <link rel="website icon" type="png" href="it2/images/est3ban-logo.png">
+</head>
+<body>
+
+<form method="post" action="forgot-password.php">
+    <br>
+    <input type="submit" value="Send">
+</form>
+
+</body>
+</html>
+
