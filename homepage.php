6bbf1233e402a48afe022a897c377915b829f0a7
esteban-cz <tomecka.stepan@gmail.com>
2022-10-21 23:07:41 +0200

    new

--- homepage.php
+++ homepage.php
@@ -1,13 +1,3 @@
 <?php
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Website</title>
-    <link rel="website icon" type="png" href="it2/images/est3ban-logo.png">
-    <link rel="stylesheet" href="it2/css/style.css">
 
-</head>
---
+++ it2/js/app.js
@@ -0,0 +1,20 @@
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function () {
+    // toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    // toggle the icon
+    this.classList.toggle("bi-eye");
+});
+
+function LightMode() {
+    var element = document.body;
+    element.classList.toggle("lightmode");
+}
+
+var DM = "Dark Mode";
+document.getElementById("switch").value = DM;
+var LM = "Light Mode";
+document.getElementById("switch").value = LM;
