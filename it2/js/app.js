53b87797510eba5c402438a9542e8bad50a6bf2d
esteban-cz <tomecka.stepan@gmail.com>
2022-10-22 11:57:08 +0200

    new

--- it2/js/app.js
+++ it2/js/app.js
@@ -12,9 +12,13 @@ togglePassword.addEventListener("click", function () {
 function LightMode() {
     var element = document.body;
     element.classList.toggle("lightmode");
+    if(document.getElementById("switch").value = DM){
+        var LM = "Light Mo";
+        document.getElementById("switch").value = LM;
+    } else{
+        var DM = "Dark Mo";
+        document.getElementById("switch").value = DM;
+    }
+
 }
 
-var DM = "Dark Mode";
-document.getElementById("switch").value = DM;
-var LM = "Light Mode";
-document.getElementById("switch").value = LM;
